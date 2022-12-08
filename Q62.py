1c81f02e54e91ce5c5478d230a6affca99bf4fa3
dkhongell <116841735+dkhongell@users.noreply.github.com>
2022-12-08 16:41:58 -0500

    Add files via upload
    No clue, i know it's wrong

---
+++ Q62.py
@@ -0,0 +1,24 @@
+class Ambulance:
+    """Represents Ambulance
+
+    attributes: priority, speed, capacity"""
+
+import math
+
+priority=0 if no patient
+priority=1 if there is a patient
+
+speed=maximum speed of Ambulance
+capacity= number of patients in Ambulance
+
+def controlled_velocity(priority, capacity, speed):
+    priority=0 if Ambulance is empty
+        if not:
+            priority=1
+    capacity= number of patients in Ambulance
+    speed= amximum speed of Ambulance
+
+controlled_velocity= -10(1- priority) +2.37(speed/10)^^3.98 +30 (capcity+1.2)
+
+print(controlled_velocity)
+    
